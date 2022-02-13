69dcbf8ce5a7f820007ec1604ff6ec069f994a23
jamiesandell <jamie.sandell@outlook.com>
2022-02-13 06:58:38 +0000

    Chapter 2 - Arrays

---
+++ Code/Chapter 02/Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names; // can reference an array of strings
+            names = new string[4]; // allocating memory for four strings in an array
+            // storing items at index positions
+        }
+    }
+}
