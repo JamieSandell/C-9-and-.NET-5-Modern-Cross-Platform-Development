fa7cc2139d07e43a329c79ef0e21461daec9dc40
jamiesandell <jamie.sandell@outlook.com>
2022-02-13 07:05:47 +0000

    Chapter 2 - Arrays

--- Code/Chapter 02/Arrays/Program.cs
+++ Code/Chapter 02/Arrays/Program.cs
@@ -9,6 +9,16 @@ namespace Arrays
             string[] names; // can reference an array of strings
             names = new string[4]; // allocating memory for four strings in an array
             // storing items at index positions
+            names[0] = "Kate";
+            names[1] = "Jack";
+            names[2] = "Rebecca";
+            names[3] = "Tom";
+            // loop through
+            for (int i = 0; i < names.Length; ++i)
+            {
+                // output the item at position i
+                Console.WriteLine(names[i]);
+            }
         }
     }
 }
