43cd2ce6e43725a424ac20967fae89a9c26e8d57
jamiesandell <jamie.sandell@outlook.com>
2022-02-20 07:46:10 +0000

    Chapter 3 - Boolean Operators

--- Code/Chapter 03/BooleanOperators/Program.cs
+++ Code/Chapter 03/BooleanOperators/Program.cs
@@ -9,8 +9,26 @@ namespace BooleanOperators
         {
             bool a = true;
             bool b = false;
-            WriteLine($"AND |   a   |   b");
-            WriteLine($"a   |   {a ")
+            WriteLine($"AND |   a     |   b");
+            WriteLine($"a   |   {a & a, -5} | {a & b, -5}");
+            WriteLine($"b   |   {b & a, -5} | {b & b, -5}");
+            WriteLine();
+            WriteLine($"OR |   a      |   b");
+            WriteLine($"a   |   {a | a, -5} | {a | b, -5}");
+            WriteLine($"b   |   {b | a, -5} | {b | b, -5}");
+            WriteLine();
+            WriteLine($"XOR |   a     |   b");
+            WriteLine($"a   |   {a ^ a, -5} | {a ^ b, -5}");
+            WriteLine($"b   |   {b ^ a, -5} | {b ^ b, -5}");
+
+            WriteLine($"a && DoStuff() = {a && DoStuff()}");
+            WriteLine($"b && DoStuff() = {b && DoStuff()}");
+        }
+
+        private static bool DoStuff()
+        {
+            WriteLine("I am doing some stuff.");
+            return true;
         }
     }
 }
