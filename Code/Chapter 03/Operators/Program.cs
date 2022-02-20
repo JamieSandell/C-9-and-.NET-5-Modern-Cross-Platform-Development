9e03487c82aa49af2d3708fdf7e3711c17a6d35b
jamiesandell <jamie.sandell@outlook.com>
2022-02-20 07:02:43 +0000

    Chapter 3 - Operators

--- Code/Chapter 03/Operators/Program.cs
+++ Code/Chapter 03/Operators/Program.cs
@@ -10,6 +10,23 @@ namespace Operators
             int a = 3;
             int b = a++;
             WriteLine($"a is {a}, b is {b}");
+
+            int c = 3;
+            int d = ++c;
+            WriteLine($"c is {c}, d is {d}");
+
+            int e = 11;
+            int f = 3;
+            WriteLine($"e is {e}, f is {f}");
+            WriteLine($"e + f = {e + f}");
+            WriteLine($"e - f = {e - f}");
+            WriteLine($"e * f = {e * f}");
+            WriteLine($"e / f = {e / f}");
+            WriteLine($"e % f = {e % f}");
+
+            double g = 11.0;
+            WriteLine($"g is {g:N1}, f is {f}");
+            WriteLine($"g / f is {g / f}");
         }
     }
 }
