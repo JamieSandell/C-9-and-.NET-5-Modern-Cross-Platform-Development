751d595e9be270dc83b4f895d31ee4322b543546
jamiesandell <jamie.sandell@outlook.com>
2022-03-06 06:55:33 +0000

    Chapter 4 - Writing Functions

--- Code/Chapter 04/WritingFunctions/Program.cs
+++ Code/Chapter 04/WritingFunctions/Program.cs
@@ -90,11 +90,81 @@ namespace WritingFunctions
                 WriteLine("You did not enter a valid amount!");
             }
         }
+        /// <summary>
+        /// Pass a 32-bit integer and it will be converted into its ordinal equivalent.
+        /// </summary>
+        /// <param name="number">cardinal value e.g. 1, 2, 3, and so on.</param>
+        /// <returns>Number as an ordinal value e.g. 1st, 2nd, 3rd, and so on.</returns>
+        static string CardinalToOrdinal(int number)
+        {
+            switch (number)
+            {
+                    case 11: // Special cases for 11th - 13th
+                    case 12:
+                    case 13:
+                        return $"{number}th";
+                    default:
+                        int lastDigit = number % 10;
+                        string suffix = lastDigit switch
+                        {
+                            1 => "st",
+                            2 => "nd",
+                            3 => "rd",
+                            _ => "th"
+                        };
+                        return $"{number}{suffix}";
+            }
+        }
+
+        static void RunCardinalToOrdinal()
+        {
+            for (int number = 1; number <= 40; ++number)
+            {
+                Write($"{CardinalToOrdinal(number)} ");
+            }
+            WriteLine();
+        }
+
+        static int Factorial(int number)
+        {
+            if (number < 1)
+            {
+                return 0;
+            }
+            else if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                checked
+                {
+                    return number * Factorial(number -1);
+                }                
+            }
+        }
+
+        static void RunFactorial()
+        {
+            for (int i = 1; i < 15; ++i)
+            {
+                try
+                {
+                    WriteLine($"{i}! = {Factorial(i):N0}");
+                }
+                catch (System.OverflowException)
+                {
+                    WriteLine($"{i}! is too big for a 32-bit integer.");
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
-            RunTimesTable();
-            RunCalculateTax();
+            //RunTimesTable();
+            //RunCalculateTax();
+            //RunCardinalToOrdinal();
+            RunFactorial();
         }
     }
 }
