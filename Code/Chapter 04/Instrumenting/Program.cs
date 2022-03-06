4203d1a9a3016300448bdf9714a2a81687e45fff
JamieSandell <jamie.sandell@outlook.com>
2022-03-06 16:53:33 +0000

    Chapter 4 - Debugging

---
+++ Code/Chapter 04/Instrumenting/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using static System.Console;
+using System.Diagnostics;
+using System.IO;
+
+namespace Instrumenting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write to a text file in the project folder
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+            Trace.AutoFlush = true; // TextWriter is buffered so this calls Flush() after writing
+            Debug.WriteLine("Debug says I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json",
+                optional: true,
+                reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            var ts = new TraceSwitch(displayName: "PacketSwitch",
+                description: "This switch is set via a JSON config.");
+            configuration.GetSection("PacketSwitch").Bind(ts);
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+        }
+    }
+}
