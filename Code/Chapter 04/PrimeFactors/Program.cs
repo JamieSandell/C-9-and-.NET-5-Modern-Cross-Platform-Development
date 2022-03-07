65a09ef6a0cb9f169ac7a6a23edc809d79020a9a
Jamie Sandell <jamie.sandell@outlook.com>
2022-03-07 13:02:07 +0000

    Exercise 4.2

---
+++ Code/Chapter 04/PrimeFactors/Program.cs
@@ -0,0 +1,20 @@
+﻿using Packt;
+using System;
+using static System.Console;
+
+namespace PrimeFactors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeFactorsLib = new PrimeFactorsLib();
+            int start = 2;
+            int end = 1000;
+            for (int number = start; number <= end; ++number)
+            {
+                WriteLine(primeFactorsLib.PrimeFactors(number));
+            }
+        }
+    }
+}
