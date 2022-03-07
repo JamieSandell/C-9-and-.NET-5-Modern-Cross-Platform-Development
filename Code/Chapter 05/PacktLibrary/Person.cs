6b105f94628cf75a483df4fc9b58cfc41d42b87b
Jamie Sandell <jamie.sandell@outlook.com>
2022-03-07 17:02:34 +0000

    Chapter 5

--- Code/Chapter 05/PacktLibrary/Person.cs
+++ Code/Chapter 05/PacktLibrary/Person.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Packt.Shared
 {
@@ -7,5 +8,26 @@ namespace Packt.Shared
         // Fields
         public string Name;
         public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+        public List<Person> Children = new List<Person>();
+        // Constants
+        public const string Species = "Homo Sapien";
+        // Read-only fields
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+        // Constructors
+        public Person()
+        {
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
     }
 }
--- Code/Chapter 05/PeopleApp/Program.cs
+++ Code/Chapter 05/PeopleApp/Program.cs
@@ -17,6 +17,73 @@ namespace PeopleApp
                 arg0: bob.Name,
                 arg1: bob.DateOfBirth
             );
+
+            var alice = new Person
+            {
+                Name = "Alive Jones",
+                DateOfBirth = new DateTime(1998, 3, 7)
+            };
+            WriteLine(
+                format: "{0} was born on {1:dd MMM yy}",
+                arg0: alice.Name,
+                arg1: alice.DateOfBirth
+            );
+
+            bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+            WriteLine(
+                format: "{0}'s favorite wonder is {1}. Its integer is {2}.",
+                arg0: bob.Name,
+                arg1: bob.FavoriteAncientWonder,
+                arg2: (int)bob.FavoriteAncientWonder
+            );
+
+            bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon |
+                WondersOfTheAncientWorld.MausolemAtHalicarnassus;
+            WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+
+            bob.Children.Add(new Person {Name = "Alfred"});
+            bob.Children.Add(new Person {Name = "Zoe"});
+            WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+            foreach (Person child in bob.Children)
+            {
+                WriteLine($"    {child.Name}");
+            }
+
+            BankAccount.InterestRate = 0.012M;
+            var jonesAccount = new BankAccount();
+            jonesAccount.AccountName = "Mrs. Jones";
+            jonesAccount.Balance = 2400;
+            WriteLine(
+                format: "{0} earned {1:C} interest.",
+                arg0: jonesAccount.AccountName,
+                arg1: jonesAccount.Balance * BankAccount.InterestRate
+            );
+            var gerrierAccount = new BankAccount();
+            gerrierAccount.AccountName = "Ms. Gerrier";
+            gerrierAccount.Balance = 98;
+            WriteLine(
+                format: "{0} earned {1:C} interest.",
+                arg0: gerrierAccount.AccountName,
+                arg1: gerrierAccount.Balance * BankAccount.InterestRate
+            );
+
+            WriteLine($"{bob.Name} is a {Person.Species}");
+
+            WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
+
+            var blankPerson = new Person();
+            WriteLine(format:
+                "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+                arg0: blankPerson.Name,
+                arg1: blankPerson.HomePlanet,
+                arg2: blankPerson.Instantiated);
+
+            var gunny = new Person("Gunny", "Mars");
+            WriteLine(format:
+                "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+                arg0: gunny.Name,
+                arg1: gunny.HomePlanet,
+                arg2: gunny.Instantiated);
         }
     }
 }
