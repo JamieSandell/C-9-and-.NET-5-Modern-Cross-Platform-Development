fab2b5c9428550214041103adc81a6fe1e4b117c
Jamie Sandell <jamie.sandell@outlook.com>
2022-03-09 16:20:30 +0000

    Exercise 5.1

---
+++ Code/Chapter 05/Exercise 5.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("1.) What are the six combinations of access modifier keywords and what do they do?");
+            WriteLine("Private - only accessible within the class or structure.");
+            WriteLine("Public - no access restricitons.");
+            WriteLine("Protected - accessible from within the class and any derived classes.");
+            WriteLine("Internal - (default) accessible from within its own assembly.");
+            WriteLine("Protected internal - same as internal plus any derived classes within the assembly.");
+            WriteLine("Private protected - Same as private, but only from within the same assembly");
+            WriteLine();
+            
+        }
+    }
+}
