2b2ec6171615fbef96988de39f9dedbb986e9ea5
Jamie Sandell <jamie.sandell@outlook.com>
2022-03-10 12:56:13 +0000

    Chapter 5 - Exercise 5.1

--- Code/Chapter 05/Exercise 5.1/Program.cs
+++ Code/Chapter 05/Exercise 5.1/Program.cs
@@ -15,7 +15,28 @@ namespace Exercise_5._1
             WriteLine("Protected internal - same as internal plus any derived classes within the assembly.");
             WriteLine("Private protected - Same as private, but only from within the same assembly");
             WriteLine();
-            
+            WriteLine("2.) What is the difference between static, const, and readonly keywords when applied to a type member?");
+            WriteLine("Static - Becomes a fixed, shared, part of the class, e.g. static int numberOfUsers = 1;");
+            WriteLine("Const - a non-changing part of the class, e.g. static float PI = 3.14f;");
+            WriteLine("Readonly - immutable, but can be set at initialisation.");
+            WriteLine();
+            WriteLine("3.) What does a constructor do?");
+            WriteLine("It is called when a new instance of an object is created.");
+            WriteLine("4.) Why should you apply the [Flags] attribute to an enum type when you want to store combined values?");
+            WriteLine("It allows you to treat the enum as a bit field, e.g. Enum Colours favouriteColours = Colours.Red | Colours.Blue;");
+            WriteLine();
+            WriteLine("5.) Why is the partial keyword useful?");
+            WriteLine("For big classes / shared projects, where it's useful to split a class over multiple files.");
+            WriteLine();
+            WriteLine("6.) What is a tuple?");
+            WriteLine("Used to store multiple items in a single variable.");
+            WriteLine();
+            WriteLine("7.) - What does the C# record keyword do?");
+            WriteLine("Creates an immutable type that provide non-destructive mutation.");
+            WriteLine();
+            WriteLine("8.) What does Overloading mean?");
+            WriteLine("Having the same function name with different signatures.");
+            WriteLine();
         }
     }
 }
