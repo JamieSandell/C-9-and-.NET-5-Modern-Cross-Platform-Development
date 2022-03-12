63f1003cf2104a0c75defee46c39f7f06a67518e
jamiesandell <jamie.sandell@outlook.com>
2022-03-12 09:38:00 +0000

    Exercise 6.2

---
+++ Code/Chapter 06/Exercise 6.2/Shape.cs
@@ -0,0 +1,9 @@
+namespace Exercise_6._2
+{
+    public class Shape
+    {
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public float Area { get; set; }
+    }
+}
