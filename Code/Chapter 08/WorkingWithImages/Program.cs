ec9f69b4c4d2034cfd2a04c8a8a07e832ae673ce
Jamie Sandell <jamie.sandell@outlook.com>
2022-03-17 14:16:26 +0000

    Chapter 8 - Working with Images

---
+++ Code/Chapter 08/WorkingWithImages/Program.cs
@@ -0,0 +1,17 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.IO;
+
+namespace WorkingWithImages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
