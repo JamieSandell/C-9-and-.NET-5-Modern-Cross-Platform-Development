f64281f3c2fcc06c934971618ee59de2de6e3bc5
Jamie Sandell <jamie.sandell@outlook.com>
2022-03-18 15:50:26 +0000

    Chapter 8 - Exercise 8.2

---
+++ Code/Chapter 08/Exercise 8.2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Create a console application that prompts the user to
+enter a regular expression, and then prompts the user to enter some input
+and compare the two for a match until the user presses Esc, as shown in the
+following output:
+
+The default regular expression checks for at least one digit.
+Enter a regular expression (or press ENTER to use the 
+default): ^[a-z]+$
+Enter some input: apples
+apples matches ^[a-z]+$
+*/
+
+using System;
+
+namespace Exercise_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
