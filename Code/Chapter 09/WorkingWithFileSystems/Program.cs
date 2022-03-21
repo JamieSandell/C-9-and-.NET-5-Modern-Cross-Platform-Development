f7f0e400fffa310221e80c8c4b1191f215f3b1a5
Jamie Sandell <jamie.sandell@outlook.com>
2022-03-21 16:59:40 +0000

    Chapter 9

---
+++ Code/Chapter 09/WorkingWithFileSystems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Directory;
+using static System.IO.Path;
+
+namespace WorkingWithFileSystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void OutputFileSystemInfo()
+        {
+            WriteLine("{0, -33} {1}", "Path.PathSeparator", PathSeparator);
+            WriteLine("{0, -33} {1}", "Path.DirectorySeparatorChar", DirectorySeparatorChar);
+            WriteLine("{0, -33} {1}", "Directory.GetCurrentDirectory()", GetCurrentDirectory());
+            WriteLine("{0, -33} {1}", "Environment.CurrentDirectory()", CurrentDirectory);
+            WriteLine("{0, -33} {1}", "Environment.SystemDirectory()", SystemDirectory);
+            WriteLine("{0, -33} {1}");
+        }
+    }
+}
