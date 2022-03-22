3e9d14aa47bd2367b15099f99862baba0c7845a9
Jamie Sandell <jamies@cyclops-electronics.com>
2022-03-22 16:55:17 +0000

    Chapter 9 - Working with Encodings

---
+++ Code/Chapter 09/WorkingWithEncodings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+
+namespace WorkingWithEncodings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Encodings");
+            WriteLine("[1] ASCII");
+            WriteLine("[2] UTF-7");
+            WriteLine("[3] UTF-8");
+            WriteLine("[4] UTF-16 (Unicode)");
+            WriteLine("[5] ");
+        }
+    }
+}
