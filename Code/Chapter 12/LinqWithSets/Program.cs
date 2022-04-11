0c9eeb0de4385690dcd3904422658f5596c8eea1
Jamie Sandell <jamies@cyclops-electronics.com>
2022-04-11 16:49:38 +0100

    Chapter 12 - Linq with Sets

---
+++ Code/Chapter 12/LinqWithSets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.Linq;
+
+namespace LinqWithSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
